058c7bb55d5cc5f6f3ac9935f224be3b7b6ab09c
ViteMamba <armkha78@gmail.com>
2025-10-16 15:17:22 +0600

    Fixed loop logic in hackerrank_day4.py

--- hackerrank_day4.py
+++ hackerrank_day4.py
@@ -1,37 +1,30 @@
 class Person:
     def __init__(self,initialAge):
         self.age = initialAge
-        
-    def age_check (self):
         if (self.age < 0):
             self.age = 0
             print("Age is not valid, setting age to 0.")
+        # Add some more code to run some checks on initialAge
     def yearPasses(self):
-        self.age+=1        
-
-    def amIold(self):
+        self.age+=1
+    def amIOld(self):
         if self.age<13:
             print("You are young")
-        elif self.age>13 and self.age<18:
-                print("You are a teenager.")
+            
+        elif self.age>=13 and self.age<18:
+            print("You are a teenager.")
+            
         else:
-                print("you are old")
+            print("you are old")
                 
-        self.yearPasses()
-        
-        if self.age<13:
-            print("You are young")
-        elif self.age>13 and self.age<18:
-                print("You are a teenager.")
-        else:
-                print("you are old")
+        # Increment the age of the person in here
 
-n = int(input())
-for i in range(n):
-    m = int(input())
-    
-    s = Person(m)
-    s.age_check()
-    
-    s.amIold()
-    
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
